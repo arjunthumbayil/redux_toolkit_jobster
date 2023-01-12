34d9c4c9b482960a12054ef711ab104b3499e644
Arjun Thumbayil <arjunthumbayil@gmail.com>
2023-01-12 15:58:28 +0530

    485. Landing Page - Structure

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,7 +3,29 @@ import main from "../assets/images/main.svg";
 import React from "react";
 
 const Landing = () => {
-  return <div>Landing Page</div>;
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="jobster logo" className="logo" />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Salvia pug venmo DIY cronut knausgaard selfies. Kombucha coloring
+            book chicharrones, meggings hammock swag forage echo park messenger
+            bag artisan woke raw denim umami. Umami listicle vinyl authentic
+            selvage freegan same.
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </main>
+  );
 };
 
 export default Landing;
