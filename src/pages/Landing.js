f31f5e8701fc23daee7829e5afed893f7c77f10b
Arjun Thumbayil <arjunthumbayil@gmail.com>
2023-01-12 16:29:51 +0530

    487. Style Landing Page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -5,28 +5,68 @@ import styled from "styled-components";
 
 const Landing = () => {
   return (
-    <main>
-      <nav>
-        <img src={logo} alt="jobster logo" className="logo" />
-      </nav>
-      <div className="container page">
-        {/* info */}
-        <div className="info">
-          <h1>
-            job <span>tracking</span> app
-          </h1>
-          <p>
-            Salvia pug venmo DIY cronut knausgaard selfies. Kombucha coloring
-            book chicharrones, meggings hammock swag forage echo park messenger
-            bag artisan woke raw denim umami. Umami listicle vinyl authentic
-            selvage freegan same.
-          </p>
-          <button className="btn btn-hero">Login/Register</button>
+    <Wrapper>
+      <main>
+        <nav>
+          <img src={logo} alt="jobster logo" className="logo" />
+        </nav>
+        <div className="container page">
+          {/* info */}
+          <div className="info">
+            <h1>
+              job <span>tracking</span> app
+            </h1>
+            <p>
+              Salvia pug venmo DIY cronut knausgaard selfies. Kombucha coloring
+              book chicharrones, meggings hammock swag forage echo park
+              messenger bag artisan woke raw denim umami. Umami listicle vinyl
+              authentic selvage freegan same.
+            </p>
+            <button className="btn btn-hero">Login/Register</button>
+          </div>
+          <img src={main} alt="job hunt" className="img main-img" />
         </div>
-        <img src={main} alt="job hunt" className="img main-img" />
-      </div>
-    </main>
+      </main>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.main`
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+  }
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+  }
+  h1 {
+    font-weight: 700;
+    span {
+      color: var(--primary-500);
+    }
+  }
+  p {
+    color: var(--grey-600);
+  }
+  .main-img {
+    display: none;
+  }
+  @media (min-width: 992px) {
+    .page {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 3rem;
+    }
+    .main-img {
+      display: block;
+    }
+  }
+`;
 
 export default Landing;
