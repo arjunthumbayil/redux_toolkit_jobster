52a3974f9c9354f164b075c981d8210318e90f72
Arjun Thumbayil <arjunthumbayil@gmail.com>
2023-07-21 07:34:53 +0530

    572. Register Setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "normalize.css";
-import "./index.css";
-import App from "./App";
+import React from 'react'
+// import ReactDOM from 'react-dom/client'
+import 'normalize.css'
+import './index.css'
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+import { createRoot } from 'react-dom/client'
+
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<App tab="home" />)
