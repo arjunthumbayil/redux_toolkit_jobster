d40b05a525384da40784bc20764fd5cb8461047d
Arjun Thumbayil <arjunthumbayil@gmail.com>
2023-07-21 12:28:29 +0530

    573. FormRow Component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react'
-// import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import 'normalize.css'
 import './index.css'
 import App from './App'
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
 
-import { createRoot } from 'react-dom/client'
+// import { createRoot } from 'react-dom/client'
 
-const container = document.getElementById('root')
-const root = createRoot(container)
-root.render(<App tab="home" />)
+// const container = document.getElementById('root')
+// const root = createRoot(container)
+// root.render(<App tab="home" />)
