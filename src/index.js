e2b94e638c5190fbbb42971741ba5528f2b4cd5f
Arjun Thumbayil <arjunthumbayil@gmail.com>
2023-07-26 12:20:26 +0530

    577. Use Slice - Setup

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 import 'normalize.css'
 import './index.css'
 import App from './App'
+// import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+import { createRoot } from 'react-dom/client'
+import { store } from './store'
+import { Provider } from 'react-redux'
+
+const container = document.getElementById('root')
+const root = createRoot(container)
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 )
-
-// import { createRoot } from 'react-dom/client'
-
-// const container = document.getElementById('root')
-// const root = createRoot(container)
-// root.render(<App tab="home" />)
